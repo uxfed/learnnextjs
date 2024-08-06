3d3b39f9991bc9ac9e00b1ac4f7e4e061585c802
uxfed <pat@uxfed.com>
2024-08-06 13:48:42 -0400

    switched

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -15,7 +15,7 @@ import { createInvoice, State } from '@/app/lib/actions';
 export default function Form({ customers }: { customers: CustomerField[] }) {
 
   const initialState: State = { message: null, errors: {} };
-  const [state, formAction] = useActionState(createInvoice, initialState);
+  const [state, formAction] = useActionState(initialState, createInvoice);
 
   return (
     <form action={formAction}>
