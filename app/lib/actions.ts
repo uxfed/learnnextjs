b1e7d66ffe41b6c515df2c6cbb86e23fcd02c739
uxfed <uxfed@users.noreply.github.com>
2024-10-11 21:06:02 -0400

    added State

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,6 +25,15 @@ const FormSchema = z.object({
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
 export async function createInvoice(prevState: State, formData: FormData) {
   const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
